1a2926dac7562ea868ae4e55d8878f689ffee138
dgalus <dawid_galus@wp.pl>
2016-01-03 13:33:48 +0100

    deleted unused files

--- policy/policy_blank.sh
+++ policy/policy_blank.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+IPTABLES=/sbin/iptables
+
+$IPTABLES -F -t nat
+$IPTABLES -P INPUT ACCEPT
+$IPTABLES -P OUTPUT ACCEPT
+$IPTABLES -P FORWARD ACCEPT
+
+$IPTABLES -F
+
 /bin/sh /opt/router/policy/static.sh
